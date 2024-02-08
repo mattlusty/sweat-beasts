c82f509946aef53e1f75823f15738503d54fcdfe
mattlusty <mattlusty123@gmail.com>
2024-02-08 19:14:57 +0000

    Working Code & Installed Google Maps and Auto Complete APIs

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 // import Navbar from "./components/Navbar";
 // import Footer from './components/Footer';
-import Wrapper from "./components/Wrapper";
 import "./App.css";
 
 function App() {
   return (
-    <Wrapper>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/cardio" element={<Cardio  />} />
-          <Route exact path="/weights" element={<Weights />} />
-          <Route exact path="/contacts" element={<Contacts />} />
-        </Routes>
-      </Router>
-    </Wrapper>
+    <Router>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route exact path="/cardio" element={<Cardio />} /> */}
+        {/* <Route exact path="/weights" element={<Weights />} /> */}
+        {/* <Route exact path="/contacts" element={<Contacts />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,5 @@
+import "./style.css";
+
+export default () => {
+  return <h1> Hello World </h1>;
+};
