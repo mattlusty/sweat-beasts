69a46c2cc6d3e44e6fbad30065944daa966b3065
mattlusty <mattlusty123@gmail.com>
2024-02-09 18:55:29 +0000

    Move Jumbotron to component

---
+++ src/components/Jumbotron/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Jumbotron = () => {
+  return (
+    <div id="jumbotron" class="p-5 mb-4 bg-body-tertiary rounded-5">
+      <h1 class="display-5 fw-bold text-center">Choose Your Workout Style!</h1>
+    </div>
+  );
+};
+
+export default Jumbotron;
