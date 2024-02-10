c8dc6c53ff5df664c7cbb93b7b652943141d3505
mattlusty <mattlusty123@gmail.com>
2024-02-10 17:03:18 +0000

    Add style to Workouts.jsx page component

--- src/pages/Workouts/index.jsx
+++ src/pages/Workouts/index.jsx
@@ -15,10 +15,14 @@ const Workouts = ({ type, muscle }) => {
   }, []);
 
   return (
-    <div id="workouts" className="container">
-      {workouts.map((w) => (
-        <WorkoutCard name={w.name} />
-      ))}
+    <div id="workouts" className="container-fluid">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3">
+        {workouts.map((w) => (
+          <div class="col py-3">
+            <WorkoutCard name={w.name} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
