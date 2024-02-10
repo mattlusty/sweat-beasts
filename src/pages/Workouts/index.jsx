d0cf9fac383cf505dfbae507a08f042cb1d79d97
mattlusty <mattlusty123@gmail.com>
2024-02-10 16:40:12 +0000

    Tidy up Workout Page component

--- src/pages/Workouts/index.jsx
+++ src/pages/Workouts/index.jsx
@@ -7,46 +7,18 @@ const Workouts = ({ type, muscle }) => {
   const [workouts, setWorkouts] = useState([]);
 
   useEffect(() => {
-    console.log("Workouts Page Component - Mounted!");
-    const doThing = async () => {
+    const updateWorkouts = async () => {
       let workouts = await api.getExercises({ type: "cardio" });
-      console.log("Workouts data recieved!");
-      console.log(workouts);
       setWorkouts(workouts);
     };
-    console.log("Do thing");
-    doThing();
-    return () => {
-      console.log("Workouts Page Component - Unmounted!");
-    };
+    updateWorkouts();
   }, []);
 
-  console.log("Workouts Page Component - Rendering");
-
-  console.log("workouts");
-  console.log(workouts);
-
-  // return (
-  //   <div id="workouts" className="container">
-  //     <div className="row row-cols-5">
-  //       <div class="col">
-  //         {workouts.map(
-  //           (w) => "hello"
-  //           // <WorkoutCard name={w.name} />
-  //         )}
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
-
   return (
-    <div id="workouts" class="container-fluid text-center">
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-        <div class="col">Column</div>
-      </div>
+    <div id="workouts" className="container">
+      {workouts.map((w) => (
+        <WorkoutCard name={w.name} />
+      ))}
     </div>
   );
 };
