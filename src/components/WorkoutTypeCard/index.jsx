a8e814e3e1ebbeda89925d357da758aba07e6d26
Michal Lasa <lasa.michal@gmail.com>
2024-02-12 21:51:36 +0000

    added to 2 grids

--- src/components/WorkoutTypeCard/index.jsx
+++ src/components/WorkoutTypeCard/index.jsx
@@ -1,9 +1,33 @@
 import "./style.css";
 
 function TypeCard() {
-    return  
+    return  (
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..."> </img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card">
+            <img src="..." className="card-img-top" alt="..."> </img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+   
+   
+
+    );
 
     
-}
+};
 
 export default TypeCard;
