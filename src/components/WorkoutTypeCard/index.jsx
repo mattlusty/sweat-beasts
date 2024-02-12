c1308be83577113f6a779d4ae86d1cdb74c303ed
Michal Lasa <lasa.michal@gmail.com>
2024-02-12 22:20:56 +0000

    created folder with images and added Cardio and weights image to code

--- src/components/WorkoutTypeCard/index.jsx
+++ src/components/WorkoutTypeCard/index.jsx
@@ -4,16 +4,16 @@ function TypeCard() {
     return  (
         <div className="row row-cols-1 row-cols-md-2 g-4">
         <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."> </img>
+          <div id ="chooseCard" className="card">
+            <img src="./src/components/WorkoutTypeCard/image/cardio.jpg" className="card-img-top" alt="Cardio"> </img>
             <div className="card-body">
               <h5 className="card-title">Cardio</h5>
             </div>
           </div>
         </div>
         <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..."> </img>
+          <div id ="chooseCard" className="card">
+            <img src="./src/components/WorkoutTypeCard/image/weights.webp" className="card-img-top" alt="Weights"> </img>
             <h5 className="card-title">Weights</h5>
           </div>
         </div>
