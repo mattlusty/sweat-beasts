79809f7dc7b3faafc472110e37472aa412a630d7
Michal Lasa <lasa.michal@gmail.com>
2024-02-12 21:58:00 +0000

    saved work

--- src/components/WorkoutTypeCard/index.jsx
+++ src/components/WorkoutTypeCard/index.jsx
@@ -7,18 +7,14 @@ function TypeCard() {
           <div className="card">
             <img src="..." className="card-img-top" alt="..."> </img>
             <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <h5 className="card-title">Cardio</h5>
             </div>
           </div>
         </div>
         <div className="col">
           <div className="card">
             <img src="..." className="card-img-top" alt="..."> </img>
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
+            <h5 className="card-title">Weights</h5>
           </div>
         </div>
       </div>
