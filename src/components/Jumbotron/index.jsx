4795b5cdbe63c5cf2b30f6566830558635f6c8c6
mattlusty <mattlusty123@gmail.com>
2024-02-13 23:04:36 +0000

    Fix classNames in Jumbotron

--- src/components/Jumbotron/index.jsx
+++ src/components/Jumbotron/index.jsx
@@ -2,8 +2,8 @@ import "./style.css";
 
 const Jumbotron = () => {
   return (
-    <div id="jumbotron" class="p-5 mb-4 bg-body-tertiary rounded-5">
-      <h1 class="display-5 fw-bold text-center">Choose Your Workout Style!</h1>
+    <div id="jumbotron" className="p-5 mb-4 bg-body-tertiary rounded-5">
+      <h1 className="display-5 fw-bold text-center">Choose Your Workout Style!</h1>
     </div>
   );
 };
