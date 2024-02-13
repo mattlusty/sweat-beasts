00f846f8c78fc03b62f1d3931c12eb4017c7c80a
Michal Lasa <lasa.michal@gmail.com>
2024-02-13 21:41:21 +0000

    finish the workouts card

--- src/components/WorkoutTypeCard/index.jsx
+++ src/components/WorkoutTypeCard/index.jsx
@@ -1,13 +1,15 @@
 import "./style.css";
 
-function WorkTypeCard({name}) {
+function WorkTypeCard({name,image}) {
     return  (
-          <div id="chooseCard" className="card">
-            <img src="./src/components/WorkoutTypeCard/image/cardio.jpg" className="card-img-top" alt="Cardio"/>
-            <div className="card-body">
-              <h5 className="card-title">{name}</h5>
+          <div id="chooseCard" className="card h-100 d-inline-block ">
+            <img src={"./src/components/WorkoutTypeCard/image/" + image } className="card-img-top h-100 d-inline-block  " alt="Cardio"/>
+            <div className="card-img-overlay  d-flex align-items-center justify-content-center">
+              <h5 className="card-title text-light  fs-1 fw-bold">{name}</h5>
             </div>
           </div>
+          
+          
     );
 
     
