51314791ce373b1d4fdeb1758f20638e7e1c2a28
mattlusty <mattlusty123@gmail.com>
2024-02-13 23:03:42 +0000

    Add state and updateWorkout function event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, useNavigation } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Home from "./pages/Home";
 import Muscles from "./pages/Muscles";
@@ -8,13 +8,22 @@ import Workouts from "./pages/Workouts";
 import "./App.css";
 
 function App() {
+  const [workouts, setWorkouts] = useState();
+  const navigation = useNavigation();
+
+  const updateWorkouts = async (query) => {
+    let workouts = await api.getExercises(query);
+    setWorkouts(workouts);
+    navigation("/workouts");
+  };
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element={<Home updateWorkouts={updateWorkouts} />} />
         {/* <Route exact path="/cardio" element={<Cardio />} /> */}
-        <Route exact path="/weights" element={<Muscles />} />
+        <Route exact path="/weights" element={<Muscles updateWorkouts={updateWorkouts} />} />
         <Route exact path="/workouts" element={<Workouts />} />
         {/* <Route exact path="/contacts" element={<Contacts />} /> */}
       </Routes>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,18 +2,18 @@ import "./style.css";
 import Jumbotron from "../../components/Jumbotron";
 import WorkoutTypeCard from "../../components/WorkoutTypeCard";
 
-const Home = () => {
+const Home = ({ updateWorkouts }) => {
   return (
     <>
       <Jumbotron />;
       <div className="container">
         <div className="row row-cols-1 row-cols-md-2 g-4">
           <div className="col">
-            <WorkoutTypeCard name="Cardio" />
+            <WorkoutTypeCard name="Cardio" onClick={() => updateWorkouts({ type: "cardio" })} />
           </div>
         </div>
         <div className="col">
-          <WorkoutTypeCard name="Weights" />
+          <WorkoutTypeCard name="Weights" updateWorkouts={updateWorkouts} />
         </div>
       </div>
     </>
