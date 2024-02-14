4a9065379a2e38d5bbcade22070afa183c81c893
mattlusty <mattlusty123@gmail.com>
2024-02-14 02:32:29 +0000

    add pics

--- src/components/WorkoutCard/index.jsx
+++ src/components/WorkoutCard/index.jsx
@@ -1,9 +1,9 @@
 import "./style.css";
 
-const WorkoutCard = ({ name }) => {
+const WorkoutCard = ({ name, image }) => {
   return (
-    <div id="workoutCard" className="card">
-      <img src="./public/images/workout2.jpg" className="card-img-top" alt="..." />
+    <div id="WorkoutCard" className="card">
+      <img src={"./public/images/" + image} className="card-img-top" alt="..." />
       <div className="card-body">
         <p className="card-text">{name}</p>
       </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 import Jumbotron from "../../components/Jumbotron";
 import WorkoutTypeCard from "../../components/WorkoutTypeCard";
 
+const cardioImages = [
+  "cardio/bike.webp",
+  "cardio/dumbell-f.jpeg",
+  "cardio/push.webp",
+  "cardio/pushup-f.webp ",
+  "cardio/ropes.jpeg ",
+  "cardio/run.jpeg ",
+  "cardio/situp-f.webp ",
+  "cardio/swim.jpeg ",
+  "cardio/tread-f.jpeg ",
+  "cardio/tread-f2.webp",
+];
+
 const Home = ({ updateWorkouts }) => {
   return (
     <>
       <Jumbotron />
 
       <div className="row row-cols-2 d-flex justify-content-center">
-        <div className="col" onClick={() => updateWorkouts({ type: "cardio" })}>
+        <div className="col" onClick={() => updateWorkouts({ type: "cardio" }, cardioImages)}>
           <WorkoutTypeCard name="Cardio" image="cardio.jpg" />
         </div>
         <Link to="/weights" className="col">
